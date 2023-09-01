c43f089667e19af99e999fd62da836722f90fd8d
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2023-08-31 19:34:13 -0700

    chore: adjust typography colors for blog posts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      typography: ({ theme }) => ({
+        white: {
+          css: {
+            "--tw-prose-body": theme("colors.white"),
+            "--tw-prose-headings": theme("colors.orange[600]"),
+            "--tw-prose-quotes": theme("colors.white"),
+            "--tw-prose-links": theme("colors.pink[500]"),
+          },
+        },
+      }),
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
