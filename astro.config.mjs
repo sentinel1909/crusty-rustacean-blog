abc9e1ccb498903e2f11690fae5f11317d378188
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2023-09-22 06:54:43 -0700

    chore: upgrade to Shuttle 0.27

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  outDir: "./server/dist",
+  outDir: "./dist",
   integrations: [tailwind(), react()],
   site: "https://blog.crusty-rustacean.dev",
 });
